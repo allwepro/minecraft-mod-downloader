f4af7802f8d417d131ece49f82cc1bb637ce6263
Dinh Marcus Nguyen <marc123nguyen@gmail.com>
2025-12-07 00:11:39 +0100

    Feature/structure (#2)
    * module/folder structure
    
    contains the module and folder structure with which we work
    additionallly each rust file contains bullet point with specs for impl
    
    * fixed typo

---
+++ src/domain/mod.rs
@@ -0,0 +1,12 @@
+/*
+This file should contain code to:
+
+ModInfo
+ModVersion
+DownloadRequest
+SearchQuery
+Trait definitions: ModRepo (search + fetch), ArtifactDownloader, VersionResolver
+ */
+
+pub fn init() {
+}
---
+++ src/utils/mod.rs
@@ -0,0 +1,11 @@
+/*
+optional utils
+    error helpers
+    reusable async helpers
+    file path helpers
+    config loading
+ */
+
+pub fn greet() -> String {
+    String::from("Hello from utils module!")
+}
