3b6fb4fb831e4e6f5776941fcd5b9476cf9738ad
Raphael Schreckeneder <44134405+allwepro@users.noreply.github.com>
2026-01-11 12:34:33 +0100

    Added mod icons

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -1,7 +1,9 @@
 mod api_service;
-mod config;
+mod config_manager;
+mod icon_service;
 mod legacy_list;
 
 pub use api_service::ApiService;
-pub use config::ConfigManager;
+pub use config_manager::ConfigManager;
+pub use icon_service::IconService;
 pub use legacy_list::LegacyListService;
