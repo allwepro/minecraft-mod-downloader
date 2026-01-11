be278b3f533f7848bac2ad95a05f1329223db995
Raphael Schreckeneder <44134405+allwepro@users.noreply.github.com>
2026-01-11 11:53:32 +0100

    Some refactoring and cleanup

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -5,6 +5,38 @@ pub use app_state::AppState;
 pub use app_ui::App;
 use serde::{Deserialize, Serialize};
 
+#[derive(PartialEq)]
+pub enum ListAction {
+    Import,
+    Duplicate,
+}
+
+#[derive(PartialEq)]
+pub enum LegacyState {
+    Idle,
+    InProgress {
+        current: usize,
+        total: usize,
+        message: String,
+    },
+    Complete {
+        suggested_name: String,
+        successful: Vec<String>,
+        failed: Vec<String>,
+        warnings: Vec<String>,
+        is_import: bool,
+    },
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DownloadStatus {
+    Idle,
+    Queued,
+    Downloading,
+    Complete,
+    Failed,
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum SortMode {
     Name,
